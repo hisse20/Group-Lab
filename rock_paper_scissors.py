04fc7a7957128bc9ad9acb629f64d9ab5fc520c8
hisse20 <hanadisse62@gmail.com>
2024-03-16 03:55:27 -0400

    Added Rock, Paper, Scissors game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,19 @@
 import random
+
+options = ["rock", "paper", "scissors"]
+computer_option = random.choice(options)
+
+print("Welcome to Group2 Rock, Paper, Scissors!")
+user_option = input("Enter your choice (rock, paper, or scissors): ")
+
+print("You chose:", user_option)
+print("Computer chose:", computer_option)
+
+if user_option == computer_option:
+    print("It's a tie!")
+elif (user_option == "rock" and computer_option == "scissors") or \
+     (user_option == "paper" and computer_option == "rock") or \
+     (user_option == "scissors" and computer_option == "paper"):
+    print("You win!")
+else:
+    print("Computer wins!")
